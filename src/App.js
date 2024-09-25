a14f879575851900f3134d20ecae8567b43da8e8
Blex1905 <122453948+Blex1905@users.noreply.github.com>
2024-09-25 14:49:51 +0200

    Full React Application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const apiKey = 'd8331c92d578125b714ce8c1915e714d'; // API-sleutel
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      const location = document.getElementById('inputLocation').value;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(location)}&appid=${apiKey}`;
+
+      axios.get(url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+          console.log(error.response.data); // Log de specifieke foutmelding van de server
+        });
+
+      setLocation(''); // Reset de locatie na het zoeken
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input
+          id="inputLocation" // ID toegevoegd aan het inputveld
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location..."
+          type="text"
+        />
+      </div>
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p> {/* Dynamisch weergeven van locatie */}
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp}K</h1> : null}
+          </div>
+          <div className="description">
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">{/* Dynamische gevoelstemperatuur weergeven */}</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">{/* Dynamische luchtvochtigheid weergeven */}</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">{/* Dynamische windsnelheid weergeven */}</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
